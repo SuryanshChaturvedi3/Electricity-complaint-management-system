1b67130ffd15eea6cf0efe34de38ea379dda6222
Suryansh Chaturvedi <suryanshchaturvedi@gmail.com>
2026-01-06 20:43:03 +0530

    feat: remove hardcoded service account path and use environment variable for Firebase initialization

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,5 @@
 require('dotenv').config(); // Load environment variables
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT || "./config/firebaseServiceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert({
