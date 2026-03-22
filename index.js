4ac8e2451c3e0961d903d66416e3fca8319588c8
Suryansh Chaturvedi <suryanshchaturvedi@gmail.com>
2026-03-23 00:45:50 +0530

    fix: update CORS origin to correct URL for deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     // Yahan '*' bilkul nahi hona chahiye
-    origin: ["http://localhost:5173", "https://electricity-complaint-management-system.onrender.com"],
+    origin: ["http://localhost:5173", "https://electricity-complaint-management-system-1.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
